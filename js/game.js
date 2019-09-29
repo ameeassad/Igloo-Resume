b98957ffc444ceec8d9fd272ee426418f855bdc9
Amee Assad <51937340+ameeassad@users.noreply.github.com>
2019-09-29 01:56:48 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,18 @@
+function loop(){
+  window.game.update();
+  requestAnimationFrame(loop);
+}
 
+class Game{
+  constructor(){
+    this.canvasElm = document.createElement("canvas");
+    this.canvasElm.width = 800;
+    this.canvasElm.height = 600;
+    
+    document.body.appendChild(this.canvasElm);
+  
+}
+  
+  update(){
+  }
+}
