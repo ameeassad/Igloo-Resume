ac7505c9c375ea7a1d3d099f446e65719c64e06f
Amee Assad <51937340+ameeassad@users.noreply.github.com>
2019-09-29 01:58:58 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -9,10 +9,14 @@ class Game{
     this.canvasElm.width = 800;
     this.canvasElm.height = 600;
     
+    this.gl = this.canvasElm.getContext("webgl2");
+    
     document.body.appendChild(this.canvasElm);
   
 }
   
   update(){
+    this.gl.viewport(0,0,this.canvasElm.width, this.canvasElm.height);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT)
   }
 }
